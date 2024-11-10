253be01095eb936d67b5605d5cfb98a85ae5a97c
oluoko <brianotieno586@gmail.com>
2024-11-10 14:53:54 +0300

    Agents In Production: updated reddit page from NBA to chess

--- src/tools/reddit.ts
+++ src/tools/reddit.ts
@@ -1,26 +1,26 @@
-import type { ToolFn } from '../../types'
-import { z } from 'zod'
-import fetch from 'node-fetch'
+import type { ToolFn } from "../../types";
+import { z } from "zod";
+import fetch from "node-fetch";
 
 export const redditToolDefinition = {
-  name: 'reddit',
+  name: "reddit",
   parameters: z.object({}),
-  description: 'get the latest posts from Reddit',
-}
+  description: "get the latest posts from Reddit",
+};
 
-type Args = z.infer<typeof redditToolDefinition.parameters>
+type Args = z.infer<typeof redditToolDefinition.parameters>;
 
 export const reddit: ToolFn<Args, string> = async ({ toolArgs }) => {
-  const { data } = await fetch('https://www.reddit.com/r/nba/.json').then(
+  const { data } = await fetch("https://www.reddit.com/r/chess/.json").then(
     (res) => res.json()
-  )
+  );
   const relevantInfo = data.children.map((child: any) => ({
     title: child.data.title,
     link: child.data.url,
     subreddit: child.data.subreddit_name_prefixed,
     author: child.data.author,
     upvotes: child.data.ups,
-  }))
+  }));
 
-  return JSON.stringify(relevantInfo, null, 2)
-}
+  return JSON.stringify(relevantInfo, null, 2);
+};
